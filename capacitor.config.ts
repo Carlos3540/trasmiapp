9053cefbd34fe7b51755e0b3bac2cfbd2976fdd0
Al3J077 <garciaalejo711@gmail.com>
2025-11-25 15:06:44 +0000

    gen APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.transmi.app',
+  appName: 'transmiapp',
+  webDir: 'out'
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. OBLIGATORIO: Genera HTML/CSS/JS estático en la carpeta 'out'
+  output: 'export',
+
+  // 2. OBLIGATORIO: Desactiva el servidor de optimización de imágenes
+  // (Sin esto, la app crasheará al intentar cargar una imagen)
+  images: {
+    unoptimized: true,
+  },
+
+  // 3. PREVENCIÓN: Desactiva el trailing slash para evitar problemas de rutas
+  trailingSlash: false,
 };
 
 export default nextConfig;
